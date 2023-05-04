9a5d94fcf2e04592f0f20b1c6bb001b6edac7dd8
Aziz Ch <98626462+M0Aziz@users.noreply.github.com>
2023-05-04 16:55:38 +0200

    Create Program.cs

---
+++ AN.UI.WEB/Program.cs
@@ -0,0 +1,36 @@
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Services;
+using AM.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IServiceFlight, ServiceFlight>();
+builder.Services.AddScoped<IServicePlane, ServicePlane>();
+//IServiceFlight = new ServiceFlight();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();//Obligatoire!!//
+//IunitOfWork UOW = new UnitOfWork(dbContext, Type);
+builder.Services.AddDbContext<DbContext,AMContext>();//Obligatoire!!//
+builder.Services
+    .AddSingleton<Type>(p=>typeof(GenericRepository<>));//Obligatoire!!//
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+app.Run();
